86fbb473f7d86383814cef7bb6d8c102d22fc0f6
michelfrechette <mifrec@hotmail.com>
2015-09-03 11:33:01 -0400

    Introducing JavaScript

---
+++ scripts.js
@@ -0,0 +1,5 @@
+console.log("Begin program!");
+alert(" hello, from the *.js file.");
+document.write("<h1>Welcome, from the *.js file.</h1>");
+alert("bye, from the *.js file.");
+console.log("Program complete!");
